d55ffa78e485ad3544f232667cb51a49ce85069b
Dávid Mørkøre <ddmorkore@hotmail.com>
2019-04-08 14:41:53 +0100

    Addaði eini boð sum verða printaði tá appin loadar

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let boð = "Hey"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(boð)
+        
     }
 
 
