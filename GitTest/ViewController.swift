7f6ff94114176d53255458aba88f950159bfc8b8
Dávid Mørkøre <ddmorkore@hotmail.com>
2019-04-08 14:13:00 +0100

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Dávid Mørkøre on 4/8/19.
+//  Copyright © 2019 Dávid Mørkøre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
