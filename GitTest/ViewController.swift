dc1f622640b5950ebf370365ce0ec67df0306c92
Dávid Mørkøre <ddmorkore@hotmail.com>
2019-04-09 13:45:30 +0100

    Havi addað label

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print(boð)
+        print("Eg havi koyrt eitt label á skermin")
         print(reverse(text: "stressed"))
         
     }
